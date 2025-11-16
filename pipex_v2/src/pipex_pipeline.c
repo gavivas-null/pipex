ec5ff84ed10fc5a7416ff0bc8c0bebf4dd304556
Gabriel Vivas <136252761+gavivas-null@users.noreply.github.com>
2025-11-16 22:42:39 +0100

    se eliminaron archivos que sobraban ya que solamente tenían una función dentro, estas se movieron a otros archivos.

--- pipex_v2/src/pipex_pipeline.c
+++ pipex_v2/src/pipex_pipeline.c
@@ -6,7 +6,7 @@
 /*   By: gavivas- <gavivas-@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/11/03 20:31:26 by gavivas-          #+#    #+#             */
-/*   Updated: 2025/11/16 20:29:42 by gavivas-         ###   ########.fr       */
+/*   Updated: 2025/11/16 22:35:57 by gavivas-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -56,4 +56,33 @@ void	execute_pipeline(t_pipex *px, char **args, int argc)
 	waitpid(pid, &px->status_b, 0);
 	while (wait(NULL) > 0)
 		;
+}
+
+void	exec_cmd(char *cmd, char **envp)
+{
+	char	**cmd_args;
+	char	*path;
+
+	cmd_args = ft_split(cmd, ' ');
+	if (!cmd_args || !cmd_args[0])
+		exit_with_error("command not found", cmd_args, NULL, 127);
+	path = get_cmd_path(cmd_args[0], envp, 0);
+	if (!path)
+		cmd_error(cmd_args[0], cmd_args);
+	if (execve(path, cmd_args, envp) == -1)
+		exit_with_error("execve", cmd_args, path, 1);
+}
+
+void	handle_command(t_pipex *px, char *cmd, int input_fd, int output_fd)
+{
+	if (dup2(input_fd, STDIN_FILENO) == -1)
+		exit_with_error("dup2 input", NULL, NULL, 1);
+	if (dup2(output_fd, STDOUT_FILENO) == -1)
+		exit_with_error("dup2 output", NULL, NULL, 1);
+	close(input_fd);
+	close(output_fd);
+	close(px->infile);
+	close(px->outfile);
+	close(px->pipefd[0]);
+	exec_cmd(cmd, px->envp);
 }
